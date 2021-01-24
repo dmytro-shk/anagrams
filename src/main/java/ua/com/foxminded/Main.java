39508da746c26d342d753437775a938e35287976
dmytro-shk <51787555+dmytro-shk@users.noreply.github.com>
2021-01-24 11:37:24 +0100

    Update Main.java

--- src/main/java/ua/com/foxminded/Main.java
+++ src/main/java/ua/com/foxminded/Main.java
@@ -1,8 +1,13 @@
 package ua.com.foxminded;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        String textInput = "a1bcd efg!h";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter your word or sentence below");
+        String textInput = sc.nextLine(); 
+        sc.close();
         System.out.println(AnagramsEngine.reverseWords(textInput));
     }
 }
